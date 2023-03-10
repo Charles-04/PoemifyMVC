ce7cd32888c271aec2a5b7cf952d65ef4d5d3a35
Charles-04 <charlesmir04@gmail.com>
2023-03-10 15:14:45 +0100

    Worked on View Models

---
+++ Poemify.BLL/Implementations/PoemService.cs
@@ -0,0 +1,32 @@
+﻿using Poemify.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poemify.BLL.Implementations
+{
+	internal class PoemService : IPoemService
+	{
+		public (bool isCreated, string messsage) CreatePoemAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public (bool isDeleted, string messsage) DeletePoemAsync(int userId, int poemId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public (bool isUpdated, string messsage) UpdatePoemAsync(int userId, int poemId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public (bool isViewed, string messsage) ViewPoemAsync(int userId, int poemId)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Poemify.BLL/Models/CreateAndUpdatePoemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Poemify.DAL.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poemify.BLL.Models
+{
+	public class CreateAndUpdatePoemViewModel
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public string Title { get; set; }
+		public string Body { get; set; }
+		public Category Category { get; set; }
+		public int Likes { get; set; }
+	}
+}
