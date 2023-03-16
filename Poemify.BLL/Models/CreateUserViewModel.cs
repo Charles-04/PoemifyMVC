1274bd7aec3e0d00442f529744356d87da34b1c9
Charles-04 <charlesmir04@gmail.com>
2023-03-16 09:55:09 +0100

    Feat : Added CreateUser DTO

--- Poemify.BLL/Models/CreateUserViewModel.cs
+++ Poemify.BLL/Models/CreateUserViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,17 @@ namespace Poemify.BLL.Models
 {
 	public class CreateUserViewModel
 	{
+		[Required]
+		public string FullName { get; set; }
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string Password { get; set; }
+		[Required]
+        public string Title { get; set; }
+		[Required]
+		public string Bio { get; set; }
+		public string IsPoet { get; set; }
 
 	}
 }
