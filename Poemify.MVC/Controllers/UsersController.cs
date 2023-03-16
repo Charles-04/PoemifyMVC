cfa6ca1af3667f08e6d3bb69a0adb887b000769d
Charles-04 <charlesmir04@gmail.com>
2023-03-16 10:20:52 +0100

    Feat : Added User Controller

---
+++ Poemify.MVC/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Poemify.BLL.Models;
+using Poemify.BLL.Interfaces;
+
+namespace Poemify.MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+                _userService= userService;
+        }
+        public IActionResult Index()
+        {
+            var model = _userService.ViewPoetsAsync();
+            return View(model);
+        }
+        public IActionResult New()
+        {
+            return View(new CreateUserViewModel());
+        }
+        public async Task<IActionResult> Save(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var (successful, msg) = await _userService.CreateUserAsync(model);
+
+                if (successful)
+                {
+
+                    TempData["SuccessMsg"] = msg;
+
+                    return RedirectToAction("Index");
+                }
+
+
+
+                ViewBag.ErrMsg = msg;
+
+                return View("New");
+
+            }
+
+            return View("New");
+
+        }
+    }
+}
