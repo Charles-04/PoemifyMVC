5df04f3b41d6de9de2a0abd59c2fdeb21fa070e2
Charles-04 <charlesmir04@gmail.com>
2023-03-16 10:01:37 +0100

    Chore : Refactored User Entity

--- Poemify.DAL/Entities/User.cs
+++ Poemify.DAL/Entities/User.cs
@@ -13,7 +13,13 @@ namespace Poemify.DAL.Entities
         public string FullName { get; set; }
         [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+
+        public string Title { get; set; } = "Poet";
+
+        [StringLength (maximumLength:500,ErrorMessage ="Bio can only be between 100 to 500 characters",MinimumLength =100)]
+        public string Bio { get; set; }
         public bool IsPoet { get;set; }
         public ICollection<Poem> Poems { get; set; }
     }
