81809fc56e3d2928620d7610085f3af3a302ef6d
Charles-04 <charlesmir04@gmail.com>
2023-03-16 09:55:54 +0100

    Feat: create user mapping profile

---
+++ Poemify.BLL/MappingProfile/UserMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Poemify.BLL.Models;
+using Poemify.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poemify.BLL.MappingProfile
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<CreateUserViewModel, User>();
+            CreateMap<User,CreateUserViewModel>();
+        }
+    }
+}
