1343bb443ead4c204a43ad33546c125ec3824854
Charles-04 <charlesmir04@gmail.com>
2023-03-16 10:06:49 +0100

    Chore : Created database migratons

--- Poemify.DAL/Migrations/20230315033321_AddedBioColumn.Designer.cs
+++ Poemify.DAL/Migrations/20230315033321_AddedBioColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Poemify.DAL.Entities;
 
@@ -11,9 +12,10 @@ using Poemify.DAL.Entities;
 namespace Poemify.DAL.Migrations
 {
     [DbContext(typeof(PoemifyDbContext))]
-    partial class PoemifyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230315033321_AddedBioColumn")]
+    partial class AddedBioColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Poemify.DAL/Migrations/20230315033321_AddedBioColumn.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Poemify.DAL.Migrations
+{
+    public partial class AddedBioColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "Users",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Users");
+        }
+    }
+}
